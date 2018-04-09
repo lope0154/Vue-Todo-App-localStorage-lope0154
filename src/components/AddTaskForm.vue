5df3c9f1224cabf49def150b5465c5fb632206e3
Juan Lopez <lope0154@algonquinlive.com>
2018-04-09 07:37:16 -0400

    First upload to github

---
+++ src/components/AddTaskForm.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <section class="dropdown">
+      <section class="dropdown-trigger">
+    <label>Due date
+      <input type="date" v-model="newTask.dueDate" class="button">
+    </label>
+    <label>Priority
+    <label class="dropdown-trigger">
+    <select v-model="newTask.priority" class="button">
+      <option value="high">high</option>
+      <option value="medium">medium</option>
+      <option value="low">low</option>
+    </select>
+    </label>
+    </label>
+    <label>Category
+    <label class="dropdown-trigger">
+    <select v-model="newTask.category" class="button">
+      <option value=" "> </option>
+      <option value="home">home</option>
+      <option value="school">school</option>
+      <option value="work">work</option>
+    </select>
+    </label>
+    </label>
+    </section>
+    </section>
+    <section>
+      <div class="control">
+      <input class="input" type="text" placeholder="Task Title" v-model.trim="newTask.title" @keyup.enter="addTask">
+      </div>
+    <button @click="addTask" class="button is-pulled-right">Add Task</button>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newTask: {}
+    }
+  },
+  created () {
+    this.newTask = this.getNewTask()
+  },
+  methods: {
+    addTask () {
+      // this.taskList.push(this.newTask)
+      this.$emit('taskAdded', this.newTask)
+      this.newTask = this.getNewTask()
+    },
+    getNewTask () {
+      const now = new Date()
+      const today = now.toISOString().substr(0, now.toISOString().indexOf('T'))
+      console.log(now)
+      return {
+        id: null,
+        category: ' ',
+        dueDate: today,
+        isComplete: false,
+        priority: 'medium',
+        title: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+section {
+  padding-top: 0.25rem;
+  padding-bottom: 0.25em;
+}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <h2 class="title is-3">{{listTitle}}</h2>
+    <div class="box">
+    <div class="toolbar, dropdown">
+      <div class="column">
+      <label for="priority-filter" class="dropdown-trigger">Show priority</label>
+      <select name="priority-filter" id="priority-filter" v-model="selectedPriority" class="button is-medium">
+        <option value="">all</option>
+        <option
+          v-for="option in priorityOptions"
+          :value="option"
+          :key="option">{{option}}</option>
+      </select>
+      </div>
+      <div class="column">
+      <label for="category-filter" class="dropdown-trigger">Show category</label>
+      <select name="category-filter" id="category-filter" v-model="selectedCategory" class="button is-medium">
+        <option value="">all</option>
+        <option
+          v-for="option in categoryOptions"
+          :value="option"
+          :key="option">{{option}}</option>
+      </select>
+      </div>
+      <div class="column is-one-quarter">
+      <label>Date</label>
+      <button @click="sortAscending = !sortAscending"  class="button is-medium is-outlined">Desc
+        <span class="icon is-medium">
+          <i class="fas fa-angle-down" aria-hidden="true"></i>
+        </span>
+        </button>
+        </div>
+    </div>
+    </div>
+    <div class="box">
+    <ul class="task-list">
+      <li v-for="task in filteredTasks" :key="task.id" class="task-item">
+        <div class="column">
+        <span class="far"
+          :class="{
+            ' fa-circle': ! task.isComplete,
+            ' fa-check-circle': task.isComplete
+          }"
+          @click="toggleDone(task)"></span>
+        </div>
+
+        <div class="column is-9">
+          <span class="title is-5">{{ task.title }}</span>
+        <section>
+          <span>{{ task.category }}</span>
+          <span>{{ task.description }}</span>
+        </section>
+        <section class="title is-6">
+          <span>{{ task.dueDate }}</span>
+        </section>
+        </div>
+        <div class="column">
+          <span>{{ task.priority }}</span>
+        </div>
+
+        <div class="column is-warning">
+        <span class="far fa-times-circle" @click="removeTask(task)"></span>
+        </div>
+      </li>
+    </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // We are not actually managing the task list in this component.
+  // We are only displaying the list. The parent component (App.vue) is
+  // passing down a reference to the list in the 'tasks' prop.
+  props: ['tasks', 'listTitle'],
+  data () {
+    return {
+      priorityOptions: ['low', 'medium', 'high'],
+      categoryOptions: [' ', 'home', 'school', 'work'],
+      selectedPriority: '',
+      selectedCategory: '',
+      sortAscending: true
+    }
+  },
+  computed: {
+    filteredPriorityTasks () {
+      // If the selectedFilter is 'all', the value is set to an empty string
+      // which will evaluate to false. In that case, we will reuturn the
+      // entire task list. Otherwise we use the array.filter() method to return
+      // only the tasks with the selected priority.
+      return (!this.selectedPriority)
+        ? this.tasks
+        : this.tasks.filter(task => task.priority === this.selectedPriority)
+    },
+    filteredCategoryTasks () {
+      return (!this.selectedCategory)
+        ? this.tasks
+        : this.tasks.filter(task => task.category === this.selectedCategory)
+    },
+    filteredTasks () {
+      const combined = [
+        ...this.filteredPriorityTasks,
+        ...this.filteredCategoryTasks
+      ]
+      const merged = [...new Set(combined)]
+      return merged.filter(task => 
+        this.filteredPriorityTasks.includes(task) &&
+        this.filteredCategoryTasks.includes(task)
+      )
+    },
+    sortedTasks () {
+      return [...this.allTasks].sort((a,b) => {
+        const dateOne = new Date(a.dueAt)
+        const dateTwo = new Date(b.dueAt)
+        return this.sortAscending ? (dateOne - dateTwo) : (dateTwo - dateOne)
+      })
+    }
+  },
+  methods: {
+    // Since this component does not own the task list data, we need to
+    // notify the parent component that the user wants to toggle the
+    // completion status of the task.  We do that by emitting a custom event,
+    // and passing up the affected task with the event. We will listen for
+    // this event in the parent component and call the appropriate method to
+    // make the change to the data.
+    toggleDone (task) {
+      this.$emit('toggleDone', task)
+    },
+    removeTask (task) {
+      this.$emit('removeTask', task)
+    }
+  }
+}
+</script>
+
+<style>
+/* We will add styles here later. */
+.task-item {
+  display: grid;
+  grid-template-columns: auto 1fr auto auto;
+  align-items: center;
+}
+.toolbar {
+  margin-bottom: 1rem;
+}
+section {
+  padding-bottom: 0.25em;
+}
+</style>
